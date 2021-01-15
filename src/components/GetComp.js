9721ffd931a3fd3479dc85153d0e0d9428f6672f
Chad Moravec <chad.moravec@colorado.edu>
2021-01-15 13:49:28 -0700

    Loading...

--- src/components/GetComp.js
+++ src/components/GetComp.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import load from "../images/load.gif";
 
 const GetComp = () => {
   const [compliment, setCompliment] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getCompliment = async () => {
     try {
-      const response = await fetch("https://compliment-server.herokuapp.com/random");
+      setLoading(true);
+      const response = await fetch("http://localhost:5000/random");
+      console.log(response);
       const jsonData = await response.json();
       setCompliment(jsonData);
+      setLoading(false);
       if (response.status === 200) {
         reveal();
       }
@@ -23,7 +28,15 @@ const GetComp = () => {
   return (
     <div className="get-comp">
       <p>Get a compliment!</p>
-      <div id="show-comp">{compliment.compliment}</div>
+      <div id="output">
+        {loading ? (
+          <div className="loading-div">
+            <img id="loading" src={load} alt="loading" />
+          </div>
+        ) : (
+          <div id="show-comp">{compliment.compliment}</div>
+        )}
+      </div>
       <button
         type="submit"
         onClick={() => getCompliment()}
